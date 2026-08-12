0833eb0a4d1190d08539b14bd4a2aac439580add
patelnirav1706 <patelnirav1706@gmail.com>
2026-08-12 22:59:30 +0530

    Add files via upload

---
+++ .py
@@ -0,0 +1,9 @@
+product = input("enter the product name:")
+price = int(input("enter the product price:"))
+quantity = int(input("enter the prodduct quantity:"))
+total = price * quantity
+
+print("product name:", product)
+print("product price:", price)
+print("product quantity:", quantity)
+print("total price:", total)
