009ccfc1b1b817016d3b0b73f989c34746cf83ec
patelnirav1706 <patelnirav1706@gmail.com>
2026-09-11 21:47:26 +0530

    Add files via upload

---
+++ day-4/hetvi.py
@@ -0,0 +1,12 @@
+marks = (int(input("enter your marks:")))
+
+if marks >= 90:
+    print("exellent")
+elif marks >= 75:
+    print("good")
+elif marks >= 50:
+    print("avrage")
+else:
+    print("fail")
+
+
